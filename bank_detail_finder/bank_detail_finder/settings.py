a6fb1d3c7856d4febbe19dad71324383e8ffdb3f
kartikeyrajvaidya <kartikeyrajvaidya@gmail.com>
2018-11-14 01:26:21 +0530

    Changed Setting.py to add postgreSQl and also added models using inspectdb

--- bank_detail_finder/bank_detail_finder/settings.py
+++ bank_detail_finder/bank_detail_finder/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'bank_detail_finder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME' : "bank_detail_finder_db",
+        'USER': 'kartikey',
+        'PASSWORD': 'kartikey',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
