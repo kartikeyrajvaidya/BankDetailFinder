c83e42e7dde0964f4cc9cc6a1e53a08f870e1ddd
kartikeyrajvaidya <kartikeyrajvaidya@gmail.com>
2018-11-16 23:45:41 +0530

    added django-heroku and made changes to pocfile

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-release: python manage.py migrate
-web: gunicorn --pythonpath bank_detail_finder bank_detail_finder.wsgi:application
+web: gunicorn bank_detail_finder.wsgi
--- bank_detail_finder/bank_detail_finder/settings.py
+++ bank_detail_finder/bank_detail_finder/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -134,3 +134,6 @@ LOGOUT_REDIRECT_URL = 'home'
 
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
